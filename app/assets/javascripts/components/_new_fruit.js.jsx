6a7ccdb10e473bc9aed31a24602c8ffb9b8fdc8e
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-26 17:19:10 +0000

    prevent default page refresh

--- app/assets/javascripts/components/_new_fruit.js.jsx
+++ app/assets/javascripts/components/_new_fruit.js.jsx
@@ -3,10 +3,11 @@ const NewFruit = (props) => {
 
   return(
     <form onSubmit={ (e) => {
+        e.preventDefault()
         props.handleFormSubmit(formFields.name.value, formFields.description.value);
         e.target.reset()
       }}>
-      
+
       <input ref={input => formFields.name = input} placeholder='Enter fruit'/>
       <input ref={input => formFields.description = input} placeholder='Enter description'/>
       <button>Submit</button>
