afd15daa232777208c38c92dfb853187e5d6b6d9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-26 17:00:43 +0000

    new fruits can be added to the db

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -5,6 +5,32 @@ class Body extends React.Component {
     this.state = {
       fruits: []
     }
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.addNewFruit = this.addNewFruit.bind(this)
+  }
+
+  handleFormSubmit(name, description) {
+    console.log(name, description);
+
+    let body = JSON.stringify({fruit: {name: name, description: description}
+    })
+
+    fetch('http://localhost:3000/api/v1/fruits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    }).then((res) => { return res.json() })
+    .then((fruit) => {
+      this.addNewFruit(fruit)
+    })
+  }
+
+  addNewFruit(fruit) {
+    this.setState({
+      fruits: this.state.fruits.concat(fruit)
+    })
   }
 
   componentDidMount() {
@@ -16,6 +42,7 @@ class Body extends React.Component {
   render() {
     return(
       <div>
+        <NewFruit handleFormSubmit={this.handleFormSubmit}/>
         <AllFruits fruits={this.state.fruits} />
       </div>
     )
---
+++ app/assets/javascripts/components/_new_fruit.js.jsx
@@ -0,0 +1,15 @@
+const NewFruit = (props) => {
+  let formFields = {}
+
+  return(
+    <form onSubmit={ (e) => {
+        props.handleFormSubmit(formFields.name.value, formFields.description.value);
+        e.target.reset()
+      }}>
+      
+      <input ref={input => formFields.name = input} placeholder='Enter fruit'/>
+      <input ref={input => formFields.description = input} placeholder='Enter description'/>
+      <button>Submit</button>
+    </form>
+  )
+}
